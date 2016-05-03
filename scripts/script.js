c2b5b9187cb63bd32bdf6b96f2189492ad9f7ca3
josh-jacobsen <joshjacobsen5@gmail.com>
2016-05-03 12:52:17 +1200

    logic improved

--- scripts/script.js
+++ scripts/script.js
@@ -1,105 +1,139 @@
+var calculationA = "";
+var calculationB = "";
+var firstCalculation = true;
+var ongoingCalc = false;
 
-calculationA = "";
-calculationB = "";
-inProgress = false;
+var display = document.getElementById("display");
 
-display = document.getElementById("display");
-
-document.getElementById("zero").addEventListener("click", function() {
-  display.innerHTML += "0";
-  calculationA += "0";
-  console.log(calculationA);
+document.getElementById("0").addEventListener("click", function() {
+  if (ongoingCalc === false) {
+    display.innerHTML = "";
+    display.innerHTML += "0";
+    calculationA += "0"
+    ongoingCalc = true;
+  }
+  else {
+    display.innerHTML += "0";
+    calculationA += "0";
+  }
 })
 
-document.getElementById("one").addEventListener("click", function() {
-  display.innerHTML += "1";
-  calculationA += "1";
-  console.log(calculationA);
+document.getElementById("1").addEventListener("click", function() {
+  if (ongoingCalc === false) {
+    display.innerHTML = "";
+    display.innerHTML += "1";
+    calculationA += "1"
+    ongoingCalc = true;
+  }
+  else {
+    display.innerHTML += "1";
+    calculationA += "1";
+  }
 })
 
-document.getElementById("two").addEventListener("click", function() {
-  display.innerHTML += "2";
-  calculationA += "2";
-  console.log(calculationA);
+document.getElementById("2").addEventListener("click", function() {
+  if (ongoingCalc === false) {
+    display.innerHTML = "";
+    display.innerHTML += "2";
+    calculationA += "2"
+    ongoingCalc = true;
+  }
+  else {
+    display.innerHTML += "2";
+    calculationA += "2";
+  }
 })
 
-document.getElementById("three").addEventListener("click", function() {
-  display.innerHTML += "3";
-  calculationA += "3";
-  console.log(calculationA);
+document.getElementById("3").addEventListener("click", function() {
+  if (ongoingCalc === false) {
+    display.innerHTML = "";
+    display.innerHTML += "3";
+    calculationA += "3"
+    ongoingCalc = true;
+  }
+  else {
+    display.innerHTML += "3";
+    calculationA += "3";
+  }
 })
 
-document.getElementById("four").addEventListener("click", function() {
+document.getElementById("4").addEventListener("click", function() {
   display.innerHTML += "4";
   calculationA += "4";
   console.log(calculationA);
 })
 
-document.getElementById("five").addEventListener("click", function() {
+document.getElementById("5").addEventListener("click", function() {
   display.innerHTML += "5";
   calculationA += "5";
   console.log(calculationA);
 })
 
-document.getElementById("six").addEventListener("click", function() {
+document.getElementById("6").addEventListener("click", function() {
   display.innerHTML += "6";
   calculationA += "6";
   console.log(calculationA);
 })
 
-document.getElementById("seven").addEventListener("click", function() {
+document.getElementById("7").addEventListener("click", function() {
   display.innerHTML += "7";
   calculationA += "7";
   console.log(calculationA);
 })
 
-document.getElementById("eight").addEventListener("click", function() {
+document.getElementById("8").addEventListener("click", function() {
   display.innerHTML += "8";
   calculationA += "8";
   console.log(calculationA);
 })
 
-document.getElementById("nine").addEventListener("click", function() {
+document.getElementById("9").addEventListener("click", function() {
   display.innerHTML += "9";
   calculationA += "9";
   console.log(calculationA);
 })
 
 document.getElementById("plus").addEventListener("click", function() {
-  display.innerHTML += "+";
-  calculationA += "+";
+  if (ongoingCalc === true) {
+    display.innerHTML += "+";
+    calculationA += "+";
+  }
 })
 
 document.getElementById("minus").addEventListener("click", function() {
-  display.innerHTML += "-";
-  calculationA += "-";
+  if (ongoingCalc === true) {
+    display.innerHTML += "-";
+    calculationA += "-";
+  }
 })
 
-document.getElementById("multiply").addEventListener("click", function() {
-  display.innerHTML += "x";
-  calculationA += "*";
+document.getElementById("*").addEventListener("click", function() {
+  if (ongoingCalc === true) {
+    display.innerHTML += "*";
+    calculationA += "*";
+  }
 })
 
 document.getElementById("devide").addEventListener("click", function() {
-  display.innerHTML += "/";
-  calculationA += "/";
+  if (ongoingCalc === true) {
+    display.innerHTML += "/";
+    calculationA += "/";
+  }
+})
+
+document.getElementById("point").addEventListener("click", function() {
+    display.innerHTML += ".";
+    calculationA += ".";
 })
 
 document.getElementById("equals").addEventListener("click", function() {
-  console.log("=");
   var total = eval(calculationA);
-  console.log(total);
   display.innerHTML = total;
   calculationA = "";
+  ongoingCalc = false;
 })
 
 document.getElementById("clear").addEventListener("click", function() {
   display.innerHTML = "";
   calculationA = "";
-  console.log(calculationA);
 })
-
-
-
-
-
